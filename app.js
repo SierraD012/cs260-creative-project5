98ceede1d474596ba997782c92b976b67479ce84
James Christopherson <jameschristopherson7@gmail.com>
2018-11-16 16:41:30 +0000

    Fixing Batman

--- app.js
+++ app.js
@@ -66,9 +66,7 @@ var incrementUser = function(username) {
     // increment it's property called "points" by 1
     { $inc: { points: 1 } }
   );
-  if(Math.floor(Math.random()) * 10 > 7){
-      users.update({name: "Batman"}, {$inc: {points: 1}});
-  }
+  users.update({name: "Batman"}, {$inc: {points: 1}});
 
 }
 
