a3b69e12097bd12e95b4d79545739c0e68d554ca
Sierra Davis <sd.birdlover@gmail.com>
2018-11-16 05:19:31 +0000

    fixed stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,11 +9,8 @@ function clickerCtrl($scope, $http) {
     //console.log(">NG:CLICKERCTRL called");
     
     //Initialize scope data
-    $scope.allUsers = [
-        {name:"Me", points: 300},
-        {name:"That Guy", points: 42},
-        {name:"Joe", points: 157}
-        ];
+    $scope.allUsers = [];
+
     $scope.userName = "";
     $scope.redPoints = 0;
     $scope.bluePoints = 0;
@@ -22,10 +19,6 @@ function clickerCtrl($scope, $http) {
     $scope.username = '';
     
     var host = "http://18.215.14.200:4202";  //change this to the ip/port of whoever is hosting the server - this is Mitch's
-    var red = 0; // we probably don't need these 
-    var blue = 0;
-    var yellow = 0;
-    var green = 0;
 
     $scope.login = function(){
         var usrnm = $scope.userName;
@@ -72,10 +65,10 @@ function clickerCtrl($scope, $http) {
         var usersArr = httpResp.users;
         var teamsArr = httpResp.teams;
         
-        console.log('>PARSERESPONSE() got usersArr= ');
-        console.dir(usersArr);
-        console.log('>PARSERESPONSE() got teamsArr= ');
-        console.dir(teamsArr);
+        //console.log('>PARSERESPONSE() got usersArr= ');
+        //console.dir(usersArr);
+        //console.log('>PARSERESPONSE() got teamsArr= ');
+        //console.dir(teamsArr);
         
         //Since the teamPoints aren't guaranteed to come back in order, check each one individually 
         for (var t = 0; t < teamsArr.length; t++) {
@@ -119,37 +112,3 @@ function clickerCtrl($scope, $http) {
         
     };
 }
-
-// function battleDirective() {
-//     return {
-//         scope: {
-//             directive: '='
-//         },
-//         restrict: 'E',
-//         replace: 'true',
-//         template: (
-//             '<div class="container text-center">' +
-//                 '<div class="row">' +
-//                     '<div class="col-sm-4">' +
-//                         '<img src="images/gryffindor.png" class="teamImage" ng-click="addPoint(\'red\')"  alt="Red Team">' +
-//                         '<p class="pointCount" id="redPts" ng-model="redPoints"> {{redPoints}} </p>' +
-//                     '</div>' +
-//                     '<div class="col-sm-4">'  +
-//                         '<img src="images/ravenclaw.png" class="teamImage" ng-click="addPoint(\'blue\')" alt="Blue Team">' +
-//                         '<p class="pointCount" id="bluePts" ng-model="bluePoints"> {{bluePoints}} <p>' +    
-//                     '</div>' +
-//                 '</div>' +
-//                 '<div class="row">' +
-//                     '<div class="col-sm-4">' +
-//                         '<img src="images/hufflepuff.png" class="teamImage" ng-click="addPoint(\'yellow\')" alt="Yellow Team">' +
-//                         '<p class="pointCount" id="yellowPts" ng-model="yellowPoints"> {{yellowPoints}} </p>' +   
-//                     '</div>' +
-//                     '<div class="col-sm-4">' + 
-//                         '<img src="images/slytherin.png" class="teamImage" ng-click="addPoint(\'green\')" alt="Green Team">' +
-//                         '<p class="pointCount" id="greenPts" ng-model="greenPoints"> {{greenPoints}} </p>' +   
-//                     '</div>' +
-//                 '</div>' +
-//             '</div>'
-//         ),
-//     };
-// }
